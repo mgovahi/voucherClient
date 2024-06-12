0a1c1c76b4ad8f209c597cce8ac8f3aaa1d99c4b
leila kefayati <104149967+leilakf@users.noreply.github.com>
2024-06-12 17:01:09 +0330

    responsive profile

--- src/app/main/pages/profile/components/LastLogin.js
+++ src/app/main/pages/profile/components/LastLogin.js
@@ -5,119 +5,156 @@ import { useTranslation } from "react-i18next";
 import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
 import { useTheme } from "@mui/material/styles";
 import {
-    Paper,
-    Button,
-    Box,
-    Typography,
-    Alert,
-    FormControl,
-    TextField,
-    IconButton,
-    Chip,
+  Paper,
+  Button,
+  Box,
+  Typography,
+  Alert,
+  FormControl,
+  TextField,
+  IconButton,
+  Chip,
+  Grid,
 } from "@mui/material";
-
-
+import FusePageSimpleHeader from "@fuse/core/FusePageSimple/FusePageSimpleHeader";
 
 const Lastlogin = () => {
-    const { t } = useTranslation();
-    const theme = useTheme();
-    const [info, setInfo] = useState({
-        SampleData
-    });
+  const { t } = useTranslation();
+  const theme = useTheme();
+  const [info, setInfo] = useState({
+    SampleData,
+  });
 
-    return (
-        <Paper
-            className="gap-4 grid my-20 "
+  return (
+    <Paper className="w-full">
+      <FusePageSimpleHeader  className="mb-12" header={t("LAST_LOGIN")}></FusePageSimpleHeader>
+
+      <Grid container>
+        {SampleData.map((dataItem) => (
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sm={6}
+            lg={12}
+            key={dataItem.id}
+            className=" text-right  sm:mx-0   "
             sx={{
-                display: "flex",
+              
+              " p:not(:first-child)": {
+                minWidth: "25%",
+                textAlign: "center",
+              },
+              display: "flex",
+              flexDirection: { xs: "column", lg: "row" },
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: 2,
+              
+            //   border:{sm:`1px solid ${theme.palette.text.grayV}`}
             }}
-        >
-            <Box>
-                <Box className="gap-x-[24px] ">
-                    <Typography
-                        variant="body1"
-                        className="font-bold text-[18px] mt-20 px-20 mb-44 "
-                        sx={{
-                            // paddingTop: "20px",
-                            height: "66px",
-                            borderBottom: 1,
-                            borderColor: "divider",
-                        }}
+          >
+            <Grid
+              className="md:mx-40 ,"
+              item
+              xs={12}
+              sm={6}
+              md={3}
+              sx={{
+                maxWidth: "100%!important",
+              }}
+            >
+              <Typography className="font-bold " sx={{ fontSize: "14px" }}>
+                <Chip
+                  color="successLight"
+                  sx={{
+                    width: "36px",
+                    height: "36px",
+                    borderRadius: "50%",
+                    mr: "10px",
+                  }}
+                  label={
+                    <FuseSvgIcon
+                      size={16}
+                      sx={{
+                        stroke: "transparent !important",
+                        display: "block!important",
+                      }}
+                      className="ml-14 "
                     >
-                        {t("LAST_LOGIN")}
-                    </Typography>
-                </Box>
-                {SampleData.map((dataItem) => (
-                    <Box
+                      mv-icons-mc:icon-Login
+                    </FuseSvgIcon>
+                  }
+                ></Chip>
+                {moment(dataItem.date).format("YYYY-MM-DD hh:mm:ss")}
+              </Typography>
+            </Grid>
 
-                        key={dataItem.id}
-                        className="flex justify-between  text-right mx-40 my-24 "
-                        sx={{
-                            " p:not(:first-child)": {
-                                minWidth: "25%",
-                                textAlign: "center",
-                            },
-                        }}
-                    >
-                        <Typography className="font-bold "
-                        sx={{fontSize: "14px",}}
-                        >
-                            <Chip
-                                color="successLight"
-                                sx={{
-                                    width: "36px",
-                                    height: "36px",
-                                    borderRadius: "50%",
-                                    mr: "10px",
-                                }}
-                                label={
-                                    <FuseSvgIcon
-                                        size={16}
-                                        sx={{ stroke: "transparent !important",display:"block!important"  }}
-                                        className="ml-14 "
-                                    >
-                                        mv-icons-mc:icon-Login
-                                    </FuseSvgIcon>
-                                }
-                            ></Chip>
-                            {moment(dataItem.date).format("YYYY-MM-DD hh:mm:ss")}
-                        </Typography>
-                        <Typography
-                            className="flex justify-center items-center	 "
-                             sx={{fontSize: "14px",color:theme.palette.text.grayT}}
-                        >{dataItem.ip}</Typography>
-                        <Typography  sx={{fontSize: "14px",color:theme.palette.text.grayT}} className="flex justify-center items-center">{dataItem.browser}</Typography>
-                        <Typography  sx={{fontSize: "14px",color:theme.palette.text.grayT}} className="flex justify-center items-center">{dataItem.os}</Typography>
-                    </Box>
-                ))}
-            </Box>
-        </Paper>
-    );
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography
+                sx={{ fontSize: "14px", color: theme.palette.text.grayT }}
+              >
+                {dataItem.ip}
+              </Typography>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={3}
+              sx={{
+                marginBottom: { sm: "10px" },
+              }}
+            >
+              <Typography
+                sx={{ fontSize: "14px", color: theme.palette.text.grayT }}
+              >
+                {dataItem.browser}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography
+                sx={{ fontSize: "14px", color: theme.palette.text.grayT }}
+              >
+                {dataItem.os}
+              </Typography>
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
+  );
 };
 
 const SampleData = [
-    {
-        ip: "۲۰۰.۱۲۳.۱.۳۰",
-        browser: "Chrome",
-        os: "Windows",
-        date: new Date().getTime(),
-        id: 1
-    },
-    {
-        ip: "۲۰۰.۱۲۳.۱.۳۰",
-        browser: "Safari",
-        os: "Mac OS",
-        date: new Date().getTime(),
-        id: 2
-    },
-    {
-        ip: "۲۰۰.۱۲۳.۱.۳۰",
-        browser: "iOS",
-        os: "iPhone Pro",
-        date: new Date().getTime(),
-        id: 3
-    },
-
+  {
+    ip: "۲۰۰.۱۲۳.۱.۳۰",
+    browser: "Chrome",
+    os: "Windows",
+    date: new Date().getTime(),
+    id: 1,
+  },
+  {
+    ip: "۲۰۰.۱۲۳.۱.۳۰",
+    browser: "Safari",
+    os: "Mac OS",
+    date: new Date().getTime(),
+    id: 2,
+  },
+  {
+    ip: "۲۰۰.۱۲۳.۱.۳۰",
+    browser: "iOS",
+    os: "iPhone Pro",
+    date: new Date().getTime(),
+    id: 3,
+  },
+  {
+    ip: "۲۰۰.۱۲۳.۱.۳۰",
+    browser: "iOS",
+    os: "iPhone Pro",
+    date: new Date().getTime(),
+    id: 3,
+  },
 ];
 
 export default Lastlogin;
